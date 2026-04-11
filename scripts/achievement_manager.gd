d8133b72e33b2630af862130d45a493a392c58e4
Tuna Göksal <114426720+Tunagoksal@users.noreply.github.com>
2026-04-11 06:21:47 +0300

    achievemnt

--- scripts/achievement_manager.gd
+++ scripts/achievement_manager.gd
@@ -4,10 +4,19 @@ signal achivement_unlocked(ach: AchievementData)
 
 @export var achievement_list: Array[AchievementData]
 
+var achievement_ui = preload("res://scenes/achievement_ui.tscn")
+
+func _ready() -> void:
+	var ui_instance = achievement_ui.instantiate()
+	add_child(ui_instance)
+	
+
 func unlock_achivement(title:String):
 	for ach in achievement_list:
 		if ach.title == title:
-			ach.unlocked = true
-			achivement_unlocked.emit(ach)
-			print_debug("achivement unlocked: ", ach.title)
+			if not ach.unlocked:
+				ach.unlocked = true
+				achivement_unlocked.emit(ach)
+				print_debug("achivement unlocked: ", ach.title)
+			return
 	
--- scripts/achievement_ui.gd
+++ scripts/achievement_ui.gd
@@ -1,14 +1,27 @@
 extends CanvasLayer
 
 @onready var panel = $PanelContainer
-@onready var icon_visual = $PanelContainer/HBoxContainer/Icon
-@onready var title_label = $PanelContainer/HBoxContainer/VBoxContainer/Title
+@onready var icon_visual = $PanelContainer/Icon
+@onready var title_label = $PanelContainer/VBoxContainer/Title
+
+var y_thing
+var x_thing
+var y_appear
 
 func _ready() -> void:
 	AchievementManager.achivement_unlocked.connect(_on_achievement_unlocked)
+	
+	var screen_size = get_window().size
+	
+	y_thing = screen_size.y + 50
+	x_thing = screen_size.x/2
+	y_appear = screen_size.y - panel.size.y * 2
+	
+	panel.global_position = Vector2(x_thing,y_thing)
 
-#TODO add some tween, anination and arrange the correct posiiton 
 func _on_achievement_unlocked(ach:AchievementData):
+	var tween  = create_tween()
+	
 	title_label.text = ach.title
 	
 	if ach.icon != null:
@@ -17,5 +30,13 @@ func _on_achievement_unlocked(ach:AchievementData):
 	else:
 		icon_visual.hide()
 		
-	panel.global_position = Vector2(400,400)
+	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(panel, "position:y", y_appear, 1.5)
+		
+	tween.tween_interval(2.0)
+	
+	tween.set_trans(Tween.TRANS_BACK)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(panel, "global_position:y", y_thing, 0.5)
 	
