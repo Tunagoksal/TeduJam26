2882dbe23eb85329e012f50cea098ee31f1d9755
Ali Deniz Sözer <113902419+alideno@users.noreply.github.com>
2026-04-12 15:08:15 +0300

    g

--- music_player.gd
+++ music_player.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var theme_player: AudioStreamPlayer = $ThemePlayer
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	theme_player.play()
