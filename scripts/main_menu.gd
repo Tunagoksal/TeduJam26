be7f05929f6e5e358bc142e3e2d9f7c6b763c3bb
eknsydm <ekinsaydam@proton.me>
2026-04-12 13:39:49 +0300

    main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,16 +6,27 @@ extends Control
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var marker: Node2D = $Marker
 
+var arrow_cursor_texture = load("res://assets/Light/Arrows/Arrow2.png")
+var point_cursor_texture = load("res://assets/Light/Hands/Hand2.png")
+var drag_cursor_texture = load("res://assets/Light/Hands/Hand_Drag2.png")
+
+
 var tween: Tween
 
 func _ready() -> void:
-	await get_tree().create_timer(0.5).timeout 
-	DialogManager.start_dialog(marker.position,["WASD for explore"],speech_sound)
+	await get_tree().create_timer(0.5).timeout
+	Input.set_custom_mouse_cursor(arrow_cursor_texture) 
+	DialogManager.start_dialog(marker.position,["WASD for explore","SHIFT for Dash"],speech_sound)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed and not event.echo:
-		if event.keycode == KEY_W or event.keycode == KEY_A or event.keycode == KEY_D or event.keycode == KEY_S:
-			DialogManager.next_line()
+		if DialogManager.current_line_index == 0:
+			if event.keycode == KEY_W or event.keycode == KEY_A or event.keycode == KEY_D or event.keycode == KEY_S:
+				DialogManager.next_line()
+		else:
+			if event.keycode == KEY_SHIFT:
+				DialogManager.next_line()
+				
 func _on_game_button_pressed() -> void:
 	SceneManager.load_new_scene(next_level_path)
 
